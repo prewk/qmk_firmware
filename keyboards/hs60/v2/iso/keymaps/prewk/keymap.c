eaddb37893079c5e8ca160b0b1510f7236824a40
Oskar Thornblad <oskar.thornblad@gmail.com>
2020-04-30 19:40:25 +0200

    Mod-Tap arrow keys

--- keyboards/hs60/v2/iso/keymaps/prewk/keymap.c
+++ keyboards/hs60/v2/iso/keymaps/prewk/keymap.c
@@ -32,16 +32,16 @@ void qdance_reset (qk_tap_dance_state_t *state, void *user_data);
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT_60_iso( /* Base layer */
-    TD(Q_DANCE),KC_1,   KC_2,     KC_3,  KC_4,  KC_5,  KC_6,    KC_7,  KC_8,  KC_9,     KC_0,     KC_MINS,  KC_EQL,  KC_BSPC,\
-    KC_TAB,   KC_Q,     KC_W,     KC_E,  KC_R,  KC_T,  KC_Y,    KC_U,  KC_I,  KC_O,     KC_P,     KC_LBRC,  KC_RBRC,         \
-    KC_GESC,  KC_A,     KC_S,     KC_D,  KC_F,  KC_G,  KC_H,    KC_J,  KC_K,  KC_L,     KC_SCLN,  KC_QUOT,  KC_NUHS, KC_ENT, \
-    KC_LSPO,  KC_GRV,   KC_Z,     KC_X,  KC_C,  KC_V,  KC_B,    KC_N,  KC_M,  KC_COMM,  KC_DOT,   KC_SLSH,           KC_RSPC,\
-    KC_LCTL,  KC_LAPO,   KC_LCMD,                      LT(_NAV, KC_SPC),                MO(_SYMB),KC_RAPC,  TG(_NAV),KC_RCTL),
+    TD(Q_DANCE),KC_1,   KC_2,     KC_3,  KC_4,  KC_5,  KC_6,    KC_7,  KC_8,  KC_9,     KC_0,               KC_MINS,       KC_EQL,  KC_BSPC,\
+    KC_TAB,   KC_Q,     KC_W,     KC_E,  KC_R,  KC_T,  KC_Y,    KC_U,  KC_I,  KC_O,     KC_P,               KC_LBRC,       KC_RBRC,         \
+    KC_GESC,  KC_A,     KC_S,     KC_D,  KC_F,  KC_G,  KC_H,    KC_J,  KC_K,  KC_L,     KC_SCLN,            KC_QUOT,       KC_NUHS, KC_ENT, \
+    KC_LSPO,  KC_GRV,   KC_Z,     KC_X,  KC_C,  KC_V,  KC_B,    KC_N,  KC_M,  KC_COMM,  KC_DOT,             KC_SLSH,                KC_RSPC,\
+    KC_LCTL,  KC_LALT,   KC_LCMD,                      LT(_NAV, KC_SPC),                LT(_SYMB, KC_LEFT), RALT_T(KC_UP), KC_DOWN, RCTL_T(KC_RIGHT)),
 
 [_SYMB] = LAYOUT_60_iso( /* Symbol layer */
     KC_NUBS,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_DEL ,\
     KC_TRNS,  KC_TRNS,  KC_UP,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  A(S(KC_8)),  A(S(KC_9))    ,\
-    KC_TRNS,  KC_LEFT,  KC_DOWN,  KC_RGHT,  S1_DEC,   S1_INC,   S2_DEC,   S2_INC,   KC_TRNS,  KC_TRNS,  KC_TRNS,  A(KC_2),  KC_TRNS,  KC_TRNS,\
+    KC_TRNS,  KC_LEFT,  KC_DOWN,  KC_RGHT,  S1_DEC,   S1_INC,   S2_DEC,   S2_INC,   KC_TRNS,  KC_TRNS,  A(KC_2),  A(KC_8),  A(KC_9),  KC_TRNS,\
     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  S(KC_7),  A(KC_7),  S(A(KC_7)),         KC_TRNS,\
     KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                                KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS),
 
