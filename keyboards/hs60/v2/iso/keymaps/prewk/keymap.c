b607ce86f5619bfe0abc2429976b09c4abf88952
Oskar Thornblad <oskar.thornblad@gmail.com>
2020-05-06 08:32:40 +0200

    Oops HJKL

--- keyboards/hs60/v2/iso/keymaps/prewk/keymap.c
+++ keyboards/hs60/v2/iso/keymaps/prewk/keymap.c
@@ -33,11 +33,11 @@ void qdance_reset (qk_tap_dance_state_t *state, void *user_data);
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT_60_iso( /* Base layer */
-    TD(Q_DANCE),KC_1,   KC_2,     KC_3,  KC_4,  KC_5,  KC_6,    KC_7,  KC_8,  KC_9,     KC_0,               KC_MINS,       KC_EQL,  KC_BSPC,\
-LT(_NUM, KC_TAB),KC_Q,  KC_W,     KC_E,  KC_R,  KC_T,  KC_Y,    KC_U,  KC_I,  KC_O,     KC_P,               KC_LBRC,       KC_RBRC,         \
-    KC_GESC,  KC_A,     KC_S,     KC_D,  KC_F,  KC_G,  KC_H,    KC_J,  KC_K,  KC_L,     KC_SCLN,            KC_QUOT,       KC_NUHS, KC_ENT, \
-    KC_LSPO,  KC_GRV,   KC_Z,     KC_X,  KC_C,  KC_V,  KC_B,    KC_N,  KC_M,  KC_COMM,  KC_DOT,             KC_SLSH,                KC_RSPC,\
-    KC_LCTL,  KC_LALT,   KC_LCMD,                      LT(_NAV, KC_SPC),                LT(_SYMB, KC_LEFT), RALT_T(KC_UP), KC_DOWN, RCTL_T(KC_RIGHT)),
+    TD(Q_DANCE),KC_1,   KC_2,     KC_3,  KC_4,  KC_5,  KC_6,    KC_7,  KC_8,  KC_9,     KC_0,               KC_MINS,         KC_EQL,  KC_BSPC,\
+LT(_NUM, KC_TAB),KC_Q,  KC_W,     KC_E,  KC_R,  KC_T,  KC_Y,    KC_U,  KC_I,  KC_O,     KC_P,               KC_LBRC,         KC_RBRC,         \
+    KC_GESC,  KC_A,     KC_S,     KC_D,  KC_F,  KC_G,  KC_H,    KC_J,  KC_K,  KC_L,     KC_SCLN,            KC_QUOT,         KC_NUHS, KC_ENT, \
+    KC_LSPO,  KC_GRV,   KC_Z,     KC_X,  KC_C,  KC_V,  KC_B,    KC_N,  KC_M,  KC_COMM,  KC_DOT,             KC_SLSH,                  KC_RSPC,\
+    KC_LCTL,  KC_LALT,   KC_LCMD,                      LT(_NAV, KC_SPC),                LT(_SYMB, KC_LEFT), RALT_T(KC_DOWN), KC_UP, RCTL_T(KC_RIGHT)),
 
 [_SYMB] = LAYOUT_60_iso( /* Symbol layer */
     KC_NUBS,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_DEL ,\
@@ -49,7 +49,7 @@ LT(_NUM, KC_TAB),KC_Q,  KC_W,     KC_E,  KC_R,  KC_T,  KC_Y,    KC_U,  KC_I,  KC
 [_NAV] = LAYOUT_60_iso( /* Nav layer */
    G(KC_NUBS),KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,      KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,      KC_F11,   KC_F12,   KC_DEL ,\
     KC_TRNS,  KC_BTN1,  KC_MS_U,  KC_BTN2,  KC_WH_D,  KC_TRNS,    KC_TRNS,  KC_PGUP,  KC_TRNS,  KC_TRNS,  KC_TRNS,     KC_TRNS,  KC_TRNS,          \
-    KC_TRNS,  KC_MS_L,  KC_MS_D,  KC_MS_R,  KC_WH_U,  A(KC_LEFT), KC_LEFT,  KC_UP,    KC_DOWN,  KC_RIGHT, A(KC_RIGHT), KC_TRNS,  KC_TRNS,  KC_TRNS,\
+    KC_TRNS,  KC_MS_L,  KC_MS_D,  KC_MS_R,  KC_WH_U,  A(KC_LEFT), KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT, A(KC_RIGHT), KC_TRNS,  KC_TRNS,  KC_TRNS,\
     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_PGDOWN,KC_TRNS,  KC_TRNS,  KC_TRNS,     KC_TRNS,            KC_TRNS,\
     KC_TRNS,  KC_TRNS,  KC_TRNS,                                  KC_TRNS,                      KC_TRNS,  KC_TRNS,     KC_TRNS,  KC_TRNS),
 
