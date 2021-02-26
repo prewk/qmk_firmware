7fe2c6c1468b603dd719ac73e1ac8d0a233dc9d2
Oskar Thornblad <oskar.thornblad@gmail.com>
2021-02-26 20:47:08 +0100

    Wat

--- keyboards/lily58/keymaps/prewk/rules.mk
+++ keyboards/lily58/keymaps/prewk/rules.mk
@@ -24,7 +24,7 @@ SRC +=  ./lib/glcdfont.c \
         ./lib/rgb_state_reader.c \
         ./lib/layer_state_reader.c \
         ./lib/logo_reader.c \
-        # ./lib/keylogger.c \
+        ./lib/keylogger.c \
         # ./lib/mode_icon_reader.c \
         # ./lib/host_led_state_reader.c \
         # ./lib/timelogger.c \
